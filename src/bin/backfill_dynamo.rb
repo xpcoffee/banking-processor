420cf3cfee525d888c38cf9b3c4e36c742863bf6
Rick Bosch <emerick.bosch@gmail.com>
2016-07-31 22:50:44 +0200

    Backfill DynamoDB with data in MySQL
    Small, temporary script to take the data in the local MySQL and smash
    it into Dynamo.

---
+++ src/bin/backfill_dynamo.rb
@@ -0,0 +1,39 @@
+# Run this in a screen - it can take a while.
+
+require_relative '../lib/banking_processor/datastore/datastore_facade'
+
+config = BankingProcessor::Config.new
+datastore = BankingProcessor::Datastore::DatastoreFacade.new(config)
+
+datastore.select_datastore('mysql')
+sql_query = "SELECT * FROM #{config.table};"
+mysql_data = datastore.query(sql_query)
+
+datastore.select_datastore('dynamo')
+puts 'Starting backfill.'
+mysql_data.each do |item|
+    account = item['account']
+    next if account.nil?
+
+    date = item['date']
+    year_month = "#{date.year}-#{date.month}"
+    day = date.day.to_s
+
+    amount = item['amount'].to_f
+    balance = item['balance'].to_f
+    description = item['description']
+
+    datastore.insert_transaction(
+        account,
+        year_month,
+        day,
+        amount,
+        balance,
+        description
+        )
+
+    print '.' # progress indicator
+    sleep(1)  # stay below Dynamo free-IOPs limit
+end
+puts ''
+puts 'Done.'
